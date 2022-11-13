01dfa733b5e4e2fa24227175d6e1ee8a79d2797a
Agustin <agusgarcia3007@gmail.com>
2022-11-12 23:45:48 -0300

    create Sidebar components and setup api endpoints

---
+++ src/constants/index.jsx
@@ -0,0 +1,41 @@
+import MusicNote from '@mui/icons-material/MusicNote'
+import Home from '@mui/icons-material/Home'
+import Code from '@mui/icons-material/Code'
+import OndemandVideo from '@mui/icons-material/OndemandVideo'
+import SportsEsports from '@mui/icons-material/SportsEsports'
+import LiveTv from '@mui/icons-material/LiveTv'
+import School from '@mui/icons-material/School'
+import FaceRetouchingNatural from '@mui/icons-material/FaceRetouchingNatural'
+import Checkroom from '@mui/icons-material/Checkroom'
+import GraphicEq from '@mui/icons-material/GraphicEq'
+import TheaterComedy from '@mui/icons-material/TheaterComedy'
+import FitnessCenter from '@mui/icons-material/FitnessCenter'
+import DeveloperMode from '@mui/icons-material/DeveloperMode'
+
+export const categories = [
+  { name: 'New', icon: <Home /> },
+  { name: 'Coding', icon: <Code /> },
+  { name: 'ReactJS', icon: <Code /> },
+  { name: 'NextJS', icon: <Code /> },
+  { name: 'Music', icon: <MusicNote /> },
+  { name: 'Education', icon: <School /> },
+  { name: 'Podcast', icon: <GraphicEq /> },
+  { name: 'Movie', icon: <OndemandVideo /> },
+  { name: 'Gaming', icon: <SportsEsports /> },
+  { name: 'Live', icon: <LiveTv /> },
+  { name: 'Sport', icon: <FitnessCenter /> },
+  { name: 'Fashion', icon: <Checkroom /> },
+  { name: 'Beauty', icon: <FaceRetouchingNatural /> },
+  { name: 'Comedy', icon: <TheaterComedy /> },
+  { name: 'Gym', icon: <FitnessCenter /> },
+  { name: 'Crypto', icon: <DeveloperMode /> },
+]
+
+export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png'
+export const demoChannelUrl = '/channel/UCmXmlB4-HJytD7wek0Uo97A'
+export const demoVideoUrl = '/video/GDa8kZLNhJ4'
+export const demoChannelTitle = 'JavaScript Mastery'
+export const demoVideoTitle =
+  'Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI'
+export const demoProfilePicture =
+  'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
---
+++ src/utils/axiosClient.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+
+const BASE_URL = 'https://youtube-v31.p.rapidapi.com/search'
+
+const options = {
+  url: BASE_URL,
+  params: {
+    relatedToVideoId: '7ghhRHRP6t4',
+    part: 'id,snippet',
+    type: 'video',
+    maxResults: '50',
+  },
+  headers: {
+    'X-RapidAPI-Key': import.meta.env.VITE_API_KEY,
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+  },
+}
